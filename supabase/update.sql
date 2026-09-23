5719a25c465fc3d653a0df22958af05b0b38eabd
Michaeljoshua1504 <michaeljoshua.p1504@gmail.com>
2026-09-23 15:13:50 +0530

    test: trigger update-supabase workflow

--- supabase/update.sql
+++ supabase/update.sql
@@ -1,4 +1,2 @@
--- Test update: touch updated_at to verify pipeline works
-UPDATE page_sections 
-SET updated_at = NOW() 
-WHERE section_key = 'visa';
+-- trigger test
+UPDATE page_sections SET updated_at = NOW() WHERE section_key = 'visa';
